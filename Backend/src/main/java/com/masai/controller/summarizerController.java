48ea1ceb25867c09236f623ef4e2d33879f557c0
kishanjalan01 <kishanjalan01@gmail.com>
2023-10-27 21:15:42 +0530

    Checks

--- Backend/src/main/java/com/masai/controller/summarizerController.java
+++ Backend/src/main/java/com/masai/controller/summarizerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.masai.service.generatorService;
 import com.masai.service.summarizerService;
 
 @RestController
